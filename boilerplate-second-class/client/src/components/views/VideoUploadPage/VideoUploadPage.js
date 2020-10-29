17819aab7ca4f410771b44d694436a9e42795b0b
harrykim14 <harrykim14@korea.ac.kr>
2020-10-30 00:03:40 +0900

    20.10.30.
    studied thumbnail upload using ffmpeg

--- boilerplate-second-class/client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ boilerplate-second-class/client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -26,6 +26,9 @@ function VideoUploadPage() {
     const [Discription, setDiscription] = useState("")
     const [Private, setPrivate] = useState(0)
     const [Category, setCategory] = useState("Film & Animation")
+    const [FilePath, setFilePath] = useState("")
+    const [Duration, setDuration] = useState("")
+    const [ThumbnailPath, setThumbnailPath] = useState("")
 
     const onTitleChange = (event) =>{
         setvideoTitle(event.currentTarget.value)
@@ -56,6 +59,24 @@ function VideoUploadPage() {
             .then(response => {
                 if(response.data.success){
                     console.log(response.data)
+
+                    let variable = {
+                        url : response.data.url,
+                        fileName : response.data.fileName
+                    }
+
+                    setFilePath(response.data.url)
+
+                    Axios.post('/api/video/thumbnail', variable)
+                        .then(response =>{
+                            if(response.data.success){
+                                console.log(response.data)
+                                setDuration(response.data.fileDuration)
+                                setThumbnailPath(response.data.url)
+                            } else {
+                                alert('썸네일 생성에 실패 했습니다.')
+                            }
+                        })
                 } else {
                     alert('비디오 업로드를 실패하였습니다.')
                 }
@@ -85,9 +106,12 @@ function VideoUploadPage() {
                     </DropZone>
 
                     {   /* Thumbnail */    }
-                    <div>
-                        <img src alt/>
-                    </div>
+                    { ThumbnailPath && 
+                        <div>
+                            <img src={`http://localhost:5000/${ThumbnailPath}`} alt="thumbnail"/>
+                        </div>
+                    }
+                    
                 </div>
             <br />
             <br />
