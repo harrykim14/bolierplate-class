e1d24f26780ac52da3ea517401fd81ff8c411241
harrykim14 <harrykim14@korea.ac.kr>
2020-11-09 14:31:40 +0900

    20. 11. 09
    Chapter 18 Like & Dislike models

---
+++ boilerplate-second-class/server/models/Dislike.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const dislikeSchema = mongoose.Schema({
+
+    userId : { 
+            type : Schema.Types.ObjectId,
+            ref: 'User'
+            },
+
+    commentId : {
+            type : Schema.Types.ObjectId,
+            ref: 'Comment'
+    },
+
+    videoId : {
+            type : Schema.Types.ObjectId,
+            ref: 'Video'
+    }
+
+}, { timestamps: true })
+
+const Dislike = mongoose.model('Comment', dislikeSchema);
+
+module.exports = { Dislike }
