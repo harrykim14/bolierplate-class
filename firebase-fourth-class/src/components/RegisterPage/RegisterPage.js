de749ab55c8c3ac45806aef8249e4efa51769621
harrykim14 <harrykim14@korea.ac.kr>
2021-01-31 03:29:18 +0900

    21. 01. 31
    Chat app wt firebase, chapter 14: regist page UI with react-bootstrap

--- firebase-fourth-class/src/components/RegisterPage/RegisterPage.js
+++ firebase-fourth-class/src/components/RegisterPage/RegisterPage.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import './style.css';
 
 function RegisterPage() {
     return (
-        <div>
-            RegisterPage
-        </div>
+    <div className="regist">
+        <h2>Register</h2>
+
+        <form>
+            <label>Email</label>
+            <input name="email" type="email" />
+
+            <label>Name</label>
+            <input name="name" />
+
+            <label>Password</label>
+            <input name="password" type="password" />
+
+            <label>Password Confirm</label>
+            <input name="password confirm" type="password" />
+
+            <input type="submit" />
+
+            <Link style={{ color:'gray', textDecoration:"none" }} to="/login">이미 아이디가 있다면...</Link>
+        </form>
+    </div>
     )
 }
 
