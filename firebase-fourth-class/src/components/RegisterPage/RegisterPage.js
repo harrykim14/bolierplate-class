2b532da8871197cfdfd33f71e1c85b421ffa78c8
harrykim14 <harrykim14@korea.ac.kr>
2021-02-02 13:37:15 +0900

    21. 02. 02
    Chat app wt Firebase, chapter 16: Why email address is not acceptable for creating user through pure firebase

--- firebase-fourth-class/src/components/RegisterPage/RegisterPage.js
+++ firebase-fourth-class/src/components/RegisterPage/RegisterPage.js
@@ -1,34 +1,62 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import firebase from '../../firebase';
 import './style.css';
 
 function RegisterPage() {
 
-    const { register, watch, errors } = useForm();
-
+    const { register, watch, errors, handleSubmit } = useForm();
+    const [errorFromSubmit, setErrorFromSubmit] = useState('');
+    const password = useRef();
+    password.current = watch("password");
     console.log(watch("email"))
+
+    const onSubmit = async (data) => {
+        try {
+            let createUser = await firebase.auth().createUserWithEmailAndPassword(data.email,data.password);
+        } catch (error) {
+            setErrorFromSubmit(error.message)
+            setTimeout(() => {
+                setErrorFromSubmit('')
+            }, 5000)
+        }
+    }
 
     return (
     <div className="regist">
         <h2>Register</h2>
 
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <label>Email</label>
             <input name="email" type="email" 
-                   ref={register({required: true, maxLength: 10})}
+                   ref={register({required: true, pattern: /^\S+@\S+$/i })}
             />
             {errors.email && <p>이 항목은 필수항목입니다.</p>}
 
             <label>Name</label>
-            <input name="name" />
+            <input name="name" 
+                   ref={register({required: true, maxLength:10 })}
+            />
+            {errors.name && errors.name.type === "required" && <p>이 항목은 필수항목입니다.</p>}
+            {errors.name && errors.name.type === "maxLength" && <p>이름은 최대 10자리까지만 작성이 가능합니다.</p>}
 
             <label>Password</label>
-            <input name="password" type="password" />
+            <input name="password" type="password"
+                   ref={register({required:true, minLength:6})}
+            />
+            {errors.password && errors.password.type === "required" && <p>이 항목은 필수항목입니다.</p>}
+            {errors.password && errors.password.type === "minLength" && <p>이름은 최소 6자리 이상이어야 합니다.</p>}
 
             <label>Password Confirm</label>
-            <input name="password confirm" type="password" />
+            <input name="passwordConfirm" type="password" 
+                   ref={register({required: true, validate: (value) => value === password.current})}
+            />
+            {errors.passwordConfirm && errors.passwordConfirm.type === "required" && <p>이 항목은 필수항목입니다.</p>}
+            {errors.passwordConfirm && errors.passwordConfirm.type === "validate" && <p>입력하신 비밀번호와 다릅니다.</p>}
 
+
+            {errorFromSubmit && <p style={{color:'red'}}>{errorFromSubmit}</p>}
             <input type="submit" />
 
             <Link style={{ color:'gray', textDecoration:"none" }} to="/login">이미 아이디가 있다면...</Link>
