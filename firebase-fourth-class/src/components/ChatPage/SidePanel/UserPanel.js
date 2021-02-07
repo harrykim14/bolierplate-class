65a1eafe5b7a357890e54b3aaf80cd722a9ba9b2
harrykim14 <harrykim14@korea.ac.kr>
2021-02-07 17:17:27 +0900

    21. 02. 07
    Chat app wt Firebase class, chapter 31: Change stored data in redux and update db data of firebase

--- firebase-fourth-class/src/components/ChatPage/SidePanel/UserPanel.js
+++ firebase-fourth-class/src/components/ChatPage/SidePanel/UserPanel.js
@@ -1,16 +1,55 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { IoMdChatboxes } from 'react-icons/io';
 import Dropdown from 'react-bootstrap/Dropdown';
 import Image from 'react-bootstrap/Image';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { setPhotoURL } from '../../../redux/actions/user_action';
+import mime from 'mime-types';
 import firebase from '../../../firebase';
 
 function UserPanel() {
 
     const user = useSelector(state => state.user.currentUser);
+    const dispatch = useDispatch();
+
+    const inputOpenImage = useRef();
 
     const handelLogout = () => {
         firebase.auth().signOut();
+    }
+
+    const handleOpenImageRef = () => {
+        inputOpenImage.current.click();
+    }
+
+    const handleUploadImage = async (event) => {
+        const file = event.target.files[0];
+        const metadata = {contentType: mime.lookup(file.name)}
+        // console.log("file", file)
+        // console.log("metadata", metadata)
+
+        try {
+            let uploadTaskSnapshot = await firebase.storage().ref()
+                .child(`user_image/${user.uid}`)
+                .put(file, metadata)
+
+            let downloadURL = await uploadTaskSnapshot.ref.getDownloadURL();
+
+            // profile image 수정
+            await firebase.auth().currentUser.updateProfile({
+                photoURL:downloadURL
+            })
+
+            dispatch(setPhotoURL(downloadURL));
+            // console.log('uploadTaskSnapshot', uploadTaskSnapshot)
+
+            await firebase.database().ref("users")
+                .child(user.uid)
+                .update({ image: downloadURL })
+
+        } catch (err) {
+            alert(err);
+        }
     }
 
     return (
@@ -25,6 +64,13 @@ function UserPanel() {
                     style = {{ width: '30px', height: '30px', marginTop: '3px' }}
                     roundedCircle />
 
+                <input type="file" 
+                       style={{display:"none"}} 
+                       ref={inputOpenImage} 
+                       accept="image/jpeg, image/png"  
+                       onChange={handleUploadImage}
+                />
+
                 <Dropdown>
                     <Dropdown.Toggle 
                         style = {{ background:'transparent', border: '0px' }}
@@ -33,7 +79,7 @@ function UserPanel() {
                     </Dropdown.Toggle>
 
                     <Dropdown.Menu>
-                        <Dropdown.Item href="#/action-1">
+                        <Dropdown.Item onClick={handleOpenImageRef}>
                             프로필 사진 변경
                         </Dropdown.Item>
                         <Dropdown.Item onClick={handelLogout}>
